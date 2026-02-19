2aac5465050b393bda0f78efc8bfb4b347b7bf6b
yaroweb <web.dako@yahoo.de>
2026-02-19 17:32:52 +0100

    feat: add SSM policy to EC2 for keyless connection

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -64,7 +64,7 @@ resource "aws_iam_role_policy" "ec2_s3" {
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  
+}  
 resource "aws_iam_instance_profile" "ec2" {
   name = "${var.project}-${var.environment}-ec2-profile"
   role = aws_iam_role.ec2.name
